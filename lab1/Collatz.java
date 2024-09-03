35df4aa7cd6caad11eddd8d2c278289e15f9a1c2
JunLang-7 <1448760937@qq.com>
2024-09-03 20:37:59 +0800

    done with Collatz

---
+++ lab1/Collatz.java
@@ -0,0 +1,21 @@
+public class Collatz {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1){
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+    }
+    /*
+    Return the next number of Collatz sequence
+     */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0){
+            return n / 2;
+        }
+        else{
+            return 3 * n + 1;
+        }
+    }
+}
