3eb55c5ac18a5a8b0150634a117cb0391e772729
JunLang-7 <1448760937@qq.com>
2024-09-20 20:34:45 +0800

    Change the comparator form lambda expression to private type in MaxArrayDeque

--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -8,18 +8,8 @@ public class MaxArrayDequeTest {
 
     @Test
     public void intMaxArrayDequeTest() {
-        Comparator<Integer> cmpInt = new Comparator<Integer>(){
-          @Override
-            public int compare(Integer o1, Integer o2) {
-              if (o1 < o2){
-                  return -1;
-              }
-              if (o1 > o2){
-                  return 1;
-              }
-              return 0;
-          }
-        };
+        // lambda expression
+        Comparator<Integer> cmpInt = (o1, o2) -> o1 - o2;
         MaxArrayDeque<Integer> intMAD = new MaxArrayDeque<>(cmpInt);
         for (int i = 0; i < 100; i++){
             intMAD.addLast(i);
@@ -33,42 +23,20 @@ public class MaxArrayDequeTest {
 
     @Test
     public void doubleMaxArrayDequeTest() {
-        Comparator<Double> cmpDouble = new Comparator<Double>(){
-          @Override
-          public int compare(Double o1, Double o2) {
-              if (o1 < o2){
-                  return -1;
-              }
-              if (o1 > o2){
-                  return 1;
-              }
-              return 0;
-          }
-        };
+        Comparator<Double> cmpDouble = MaxArrayDeque.getDoubleComparator();
         MaxArrayDeque<Double> doubleMAD = new MaxArrayDeque<>(cmpDouble);
         for (int i = 0; i <= 100; i++){
             doubleMAD.addFirst(i / 2.0);
         }
-        assertEquals((double) 50.0, (double)doubleMAD.max(), 1e-7);
+        assertEquals(50.0, doubleMAD.max(), 1e-7);
         doubleMAD.addLast(100.0);
         doubleMAD.addLast(100.0);
-        assertEquals((double) 100.0, (double) doubleMAD.max(), 1e-7);
+        assertEquals(100.0, doubleMAD.max(), 1e-7);
     }
 
     @Test
     public void longMaxArrayDequeTest() {
-        Comparator<Long> cmpLong = new Comparator<Long>(){
-            @Override
-            public int compare(Long o1, Long o2) {
-                if (o1 < o2){
-                    return -1;
-                }
-                if (o1 > o2){
-                    return 1;
-                }
-                return 0;
-            }
-        };
+        Comparator<Long> cmpLong = MaxArrayDeque.getLongComparator();
         MaxArrayDeque<Long> longMAD = new MaxArrayDeque<>(cmpLong);
         for (long i = 1000000; i <= 10000000; i++){
             longMAD.addLast(i);
@@ -78,12 +46,7 @@ public class MaxArrayDequeTest {
 
     @Test
     public void stringMaxArrayDequeTest() {
-        Comparator<String> cmpString = new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        };
+        Comparator<String> cmpString = MaxArrayDeque.getStringComparator();
         MaxArrayDeque<String> stringMaxArrayDeque = new MaxArrayDeque<>(cmpString);
         for (char i = 'a'; i <= 'z'; i++){
             stringMaxArrayDeque.addLast(String.valueOf(i));
@@ -93,18 +56,8 @@ public class MaxArrayDequeTest {
 
     @Test
     public void MaxArrayDequeTestWithComparator() {
-        Comparator<Integer> cmpInt = new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        };
-        Comparator<Integer> cmpIntAbs = new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return Math.abs(o1 - o2);
-            }
-        };
+        Comparator<Integer> cmpInt = MaxArrayDeque.getIntComparator();
+        Comparator<Integer> cmpIntAbs = MaxArrayDeque.getAbsIntComparator();
         MaxArrayDeque<Integer> intMAD = new MaxArrayDeque<>(cmpInt);
         for (int i = 0; i < 100; i++){
             intMAD.addLast(i * (int) Math.pow(-1, i));
