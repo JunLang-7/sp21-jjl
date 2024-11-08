b2e587e282847ea425bba822aef44c4d0a569fa3
JunLang-7 <1448760937@qq.com>
2024-11-08 11:55:06 +0800

    Finish Lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,100 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    private static class Position {
+        public int x;
+        public int y;
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * add a hexagon to the tile world
+     * @param tiles the tiles World
+     * @param side the side length of the hexagon
+     * @param seed the random seed
+     * @param p the position of the center
+     */
+    public static void addHexgon(TETile[][] tiles, int side, long seed, Position p) {
+        TETile randomTileType = randomTile();
+        // draw the upper part
+        int cnt = side;
+        for (int i = 0; i < side; i++) {
+            drawLine(tiles,side + 2 * i, randomTileType, p.x, p.y + cnt);
+            cnt--;
+        }
+        // draw the lower part
+        cnt = 0;
+        for (int i = side-1; i >= 0; i--) {
+            drawLine(tiles,side + 2 * i, randomTileType, p.x, p.y - cnt);
+            cnt++;
+        }
+    }
+
+    /**
+     * Picks a RANDOM tile in the with the rand number
+     * @return the Tileset type
+     */
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(4);
+        switch (tileNum) {
+            case 0: return Tileset.FLOWER;
+            case 1: return Tileset.GRASS;
+            case 2: return Tileset.MOUNTAIN;
+            case 3: return Tileset.SAND;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    /**
+     * Decompose the hexagon into line and draw the line
+     * @param tiles the tiles World
+     * @param line the line
+     * @param type the random Tileset type
+     * @param posX the middle x position
+     * @param posY the middle y position
+     */
+    private static void drawLine(TETile[][] tiles, int line, TETile type, int posX, int posY) {
+        for (int x = posX - line / 2; x <= posX + line / 2; x++) {
+            tiles[x][posY] = type;
+        }
+    }
+
+    /**
+     * get the next right position of the Hexagon
+     * @param pre the pre position
+     * @param side the side of the hexagon
+     * @return
+     */
+    private static Position nextRight(Position pre, int side) {
+        return new Position(pre.x + 2 * side - 1, pre.y + side);
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] tiles = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+        int centerX = WIDTH / 2;
+        int centerY = HEIGHT / 2;
+        Position p = new Position(centerX, centerY);
+        addHexgon(tiles, 3, SEED, p);
+        p = nextRight(p, 3);
+        addHexgon(tiles, 3, SEED, p);
+
+        ter.renderFrame(tiles);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -5,21 +5,26 @@ After looking at your own implementation, consider the implementation provided n
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
 Answer:
-
+By using formulation to get the next right position with the method nextRight()<br>
+I don't really understand it until I search for implements in Github, which inspires me a lot, and I believe I have organised it well. 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
 Answer:
+Yes, the hexagon is somehow like the room and hallway in different shape, and the nextRight() method is the same thing to ensure each room and hallway is not isolated forever.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
 Answer:
+Maybe just to organize like the nextRight() method with just interface, which doesn't take so many details but help better understand and link the program.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
 Answer:
+Difference:Maybe the width, which hallway has a limitation but room does not, and the hallway can turn while room are just specific shape<br>
+Common: they both have to link each other, and all must reachable.
